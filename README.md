20ee7cc2d25baa6b160e73a231c80d0025529184
Gregory Norton <gregory.norton@me.com>
2023-07-11 01:11:54 -0400

    Added initial framework

---
+++ README.md
@@ -0,0 +1,2 @@
+## Youtube-playing discord bot
+
---
+++ __main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import sys
+import requests
+
+from youtube_dl import YoutubeDownloade
+
+if __name__ == '__main__':
+	sys.exit(main(sys.argv))
+
